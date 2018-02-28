6f4c16ed71e540f654c0cbeebee9991875490c4a
bharath <bharathp@biarca.com>
2018-02-28 16:39:36 +0530

    Completed function of seprating files into folders based on id

--- csv_utilites/main.py
+++ csv_utilites/main.py
@@ -1,8 +1,7 @@
 import csv
 import utils_csv as us
-#with open('test.csv', 'rb')as csvfile:
-#    reader = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
-#    for row in reader:
-#        print ','.join(row)
 
-print us.create_file( us.categorize_items(us.read_csv('test.csv', 'rb', ' '), 0))
+file_list =  us.create_file( us.categorize_items(us.read_csv('test.csv', 'rb', ' '), 0))
+us.copy_files_into_folders(file_list, "test.csv")
+  
+
